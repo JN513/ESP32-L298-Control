63108f5caec4fc09208c862071f580c53a7cf243
Julio <jn5.guape@gmail.com>
2021-03-10 21:43:46 -0300

    corrigindo erros

--- Motors.cpp
+++ Motors.cpp
@@ -1,16 +1,16 @@
 #include "Motors.h"
 
-Motors :: Motors(int M1a, int M1b, int M2a, int M2b, int _speed){
+Motors :: Motors( int M1a, int M1b, int M2a, int M2b, int _speed ){
     this->m1a = M1a;
     this->m1b = M1b;
     this->m2a = M2a;
     this->m2b = M2b;
     this->speed = _speed;
 
-    pinMode(M1a, OUTPUT);
-    pinMode(M1b, OUTPUT);
-    pinMode(M2a, OUTPUT);
-    pinMode(M2b, OUTPUT);
+    pinMode( M1a, OUTPUT );
+    pinMode( M1b, OUTPUT );
+    pinMode( M2a, OUTPUT );
+    pinMode( M2b, OUTPUT );
 }
 
 void Motors :: set_speed(int _speed){
@@ -22,132 +22,132 @@ void Motors :: set_speed(int _speed){
 }
 
 void Motors :: stop(){
-    digitalWrite(m1a, HIGH);
-    digitalWrite(m1b, HIGH);
-    digitalWrite(m2a, HIGH);
-    digitalWrite(m2b, HIGH);
+    digitalWrite( this->m1a, HIGH );
+    digitalWrite( this->m1b, HIGH );
+    digitalWrite( this->m2a, HIGH );
+    digitalWrite( this->m2b, HIGH );
 }
 
 void Motors :: neutral(){
-    digitalWrite(m1a, LOW);
-    digitalWrite(m1b, LOW);
-    digitalWrite(m2a, LOW);
-    digitalWrite(m2b, LOW);
+    digitalWrite( this->m1a, LOW );
+    digitalWrite( this->m1b, LOW );
+    digitalWrite( this->m2a, LOW );
+    digitalWrite( this->m2b, LOW );
 }
 
 void Motors :: neutral_a(){
-    digitalWrite(m1a, LOW);
-    digitalWrite(m1b, LOW);
+    digitalWrite( this->m1a, LOW );
+    digitalWrite( this->m1b, LOW );
 }
 
 void Motors :: neutral_b(){
-    digitalWrite(m2a, LOW);
-    digitalWrite(m2b, LOW);
+    digitalWrite( this->m2a, LOW );
+    digitalWrite( this->m2b, LOW );
 }
 
 void Motors :: stop_a(){
-    digitalWrite(m1a, HIGH);
-    digitalWrite(m1b, HIGH);
+    digitalWrite( this->m1a, HIGH );
+    digitalWrite( this->m1b, HIGH );
 }
 
 void Motors :: stop_b(){
-    digitalWrite(m2a, HIGH);
-    digitalWrite(m2b, HIGH);
+    digitalWrite( this->m2a, HIGH );
+    digitalWrite( this->m2b, HIGH );
 }
 
 void Motors :: front_ma(int _speed){
-    direction_a = 1;
+    this->direction_a = 1;
     if(_speed == 0){
-        digitalWrite(m1a, LOW);
-        analogWrite(m1b, _speed);
+        digitalWrite( this->m1a, LOW );
+        analogWrite( this->m1b, _speed );
     }else{
-        digitalWrite(m1a, LOW);
-        analogWrite(m1b, speed);
+        digitalWrite( this->m1a, LOW );
+        analogWrite( this->m1b, speed );
     }
 }
 
 void Motors :: behind_ma(int _speed){
-    direction_a = 0;
+    this->direction_a = 0;
     if(_speed == 0){
-        analogWrite(m1a, _speed);
-        digitalWrite(m1b, LOW);
+        analogWrite( this->m1a, _speed );
+        digitalWrite( this->m1b, LOW );
     }else{
-        analogWrite(m1a, speed);
-        digitalWrite(m1b, LOW);
+        analogWrite( this->m1a, speed );
+        digitalWrite( this->m1b, LOW );
     }
 }
 
 void Motors :: front_mb(int _speed){
-    direction_b = 1;
+    this->direction_b = 1;
     if(_speed == 0){
-        digitalWrite(m2a, LOW);
-        analogWrite(m2b, _speed);
+        digitalWrite( this->m2a, LOW );
+        analogWrite( this->m2b, _speed );
     }else{
-        digitalWrite(m2a, LOW);
-        analogWrite(m2b, speed);        
+        digitalWrite( this->m2a, LOW );
+        analogWrite( this->m2b, speed );        
     }
 }
 
 void Motors :: behind_mb(int _speed){
-    direction_b = 0;
+    this->direction_b = 0;
     if(_speed == 0){
-        analogWrite(m2a, _speed);
-        digitalWrite(m2b, LOW);
+        analogWrite( this->m2a, _speed );
+        digitalWrite( this->m2b, LOW );
     }else{
-        analogWrite(m2a, speed);
-        digitalWrite(m2b, LOW);
+        analogWrite( this->m2a, speed );
+        digitalWrite( this->m2b, LOW );
     }
 }
 
 void Motors :: front(int _speed){
-    direction_a = 1;
-    direction_b = 1;
+    this->direction_a = 1;
+    this->direction_b = 1;
         if(_speed == 0){
-        digitalWrite(m1a, LOW);
-        analogWrite(m1b, _speed);
-        digitalWrite(m2a, LOW);
-        analogWrite(m2b, _speed);
+        digitalWrite( this->m1a, LOW );
+        analogWrite( this->m1b, _speed );
+        digitalWrite( this->m2a, LOW );
+        analogWrite( this->m2b, _speed );
     }else{
-        digitalWrite(m1a, LOW);
-        analogWrite(m1b, speed);
-        digitalWrite(m2a, LOW);
-        analogWrite(m2b, speed);
+        digitalWrite( this->m1a, LOW );
+        analogWrite( this->m1b, speed );
+        digitalWrite( this->m2a, LOW );
+        analogWrite( this->m2b, speed );
     }
 }
 
 void Motors :: behind(int _speed){
-    direction_a = 0;
-    direction_b = 0;
+    this->direction_a = 0;
+    this->direction_b = 0;
     if(_speed == 0){
-        analogWrite(m1a, _speed);
-        digitalWrite(m1b, LOW);
-        analogWrite(m2a, _speed);
-        digitalWrite(m2b, LOW);
+        analogWrite( this->m1a, _speed );
+        digitalWrite( this->m1b, LOW );
+        analogWrite( this->m2a, _speed );
+        digitalWrite( this->m2b, LOW );
     }else{
-        analogWrite(m1a, speed);
-        digitalWrite(m1b, LOW);
-        analogWrite(m2a, speed);
-        digitalWrite(m2b, LOW);
+        analogWrite( this->m1a, speed );
+        digitalWrite( this->m1b, LOW );
+        analogWrite( this->m2a, speed);
+        digitalWrite( this->m2b, LOW );
     }
 }
 
 int Motors :: maximize_speed(){
-    speed = 4095;
+    this->speed = 4095;
     return speed;
 }
 
 int Motors :: minimize_speed(){
-    speed = 10;
-    return speed;
+    this->speed = 10;
+    return this->speed;
 }
 
-int get_speed(){
-    return speed;
+int Motors :: get_speed(){
+    return this->speed;
 }
 
 int Motors :: get_direction_a(){
-    return direction_a;
+    return this->direction_a;
 }
 int Motors :: get_direction_b(){
-    return direction_b;
+    return this->direction_b;
 }
