03cb4d11492ce28b1c32dd5a8b193b15350a6b9a
Julio <jn5.guape@gmail.com>
2021-03-11 10:48:18 -0300

    novas funções

--- Motors.h
+++ Motors.h
@@ -5,8 +5,11 @@
 
 class Motors{
     public:
-        Motors(int M1a, int M1b, int M2a, int M2b, int _speed = 2000);
-        void set_speed(int _speed);
+        Motors(int M1a, int M1b, int M2a, int M2b, int _speed = 2000, int _freq=1000, int _resolution=12, int _ch0=0, int _ch1=1, int _ch2=2, int _ch3=3);
+        int set_freq(int _freq);
+        int set_resolution(int _resolution);
+        int set_speed(int _speed);
+        void set_channels(int _ch0, int _ch1, int _ch2, int _ch3);
         void stop();
         void neutral();
         void neutral_ma();
@@ -30,6 +33,9 @@ class Motors{
         int speed;
         int direction_a = 1;
         int direction_b = 1;
+        int freq;
+        int resolution;
+        int ch0, ch1, ch2, ch3;
 };
 
 #endif
